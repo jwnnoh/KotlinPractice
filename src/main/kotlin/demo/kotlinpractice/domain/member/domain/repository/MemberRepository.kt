f8cff8c86edff7bb71a99837297baf6e80ba9968
노정완 <jeongwan.noh@gmail.com>
2024-11-18 21:35:50 +0900

    fix: 빈 등록을 위한 어노테이션 추가

--- src/main/kotlin/demo/kotlinpractice/domain/member/domain/repository/MemberRepository.kt
+++ src/main/kotlin/demo/kotlinpractice/domain/member/domain/repository/MemberRepository.kt
@@ -3,6 +3,7 @@ package demo.kotlinpractice.domain.member.domain.repository
 import demo.kotlinpractice.domain.member.domain.Member
 
 interface MemberRepository {
+
     fun findById(id: Long): Member
 
     fun existsByName(name: String): Boolean
--- src/main/kotlin/demo/kotlinpractice/domain/member/infra/repository/MemberJPARepositoryAdaptor.kt
+++ src/main/kotlin/demo/kotlinpractice/domain/member/infra/repository/MemberJPARepositoryAdaptor.kt
@@ -3,8 +3,10 @@ package demo.kotlinpractice.domain.member.infra.repository
 import demo.kotlinpractice.domain.member.domain.Member
 import demo.kotlinpractice.domain.member.domain.repository.MemberRepository
 import demo.kotlinpractice.domain.member.infra.MemberEntity
+import org.springframework.stereotype.Repository
 import kotlin.jvm.optionals.getOrNull
 
+@Repository
 class MemberJPARepositoryAdaptor(
     private val memberJPARepository: MemberJPARepository
 ) : MemberRepository {
