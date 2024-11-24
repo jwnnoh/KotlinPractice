f0188dddafd76c7d73fb5fad9f8a349796446378
노정완 <jeongwan.noh@gmail.com>
2024-11-24 15:26:37 +0900

    feat: JWT 생성 로직 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,22 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    //jjwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.mockk:mockk:1.13.5")
+    testRuntimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.security:spring-security-test")
 
     // kotest
     testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
---
+++ src/main/kotlin/demo/kotlinpractice/global/jwt/JwtProvider.kt
@@ -0,0 +1,56 @@
+package demo.kotlinpractice.global.jwt
+
+import demo.kotlinpractice.domain.auth.AuthDetailsService
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.Base64
+import java.util.Date
+import javax.crypto.spec.SecretKeySpec
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+
+@Service
+class JwtProvider(
+    private val authDetailsService: AuthDetailsService,
+    @Value("\${jwt.secret}") private val secret: String
+) {
+    private val accessTokenDuration: Duration = 1.days
+
+    private val secretKey: SecretKeySpec
+        get() {
+            val keyBytes: ByteArray = Base64.getDecoder().decode(secret)
+            return SecretKeySpec(keyBytes, "HmacSHA256")
+        }
+
+    fun generateAccessToken(memberId: Long, name: String, role: String): String {
+        return generateToken(memberId, name, accessTokenDuration, role)
+    }
+
+    private fun generateToken(
+        memberId: Long,
+        name: String,
+        duration: Duration,
+        role: String
+    ): String {
+        val now = Date()
+        val expiry = Date(now.time + duration.inWholeMilliseconds)
+
+        return Jwts.builder()
+            .subject(name)
+            .claim("id", memberId)
+            .claim("role", role)
+            .expiration(expiry)
+            .signWith(secretKey)
+            .compact()
+    }
+
+    fun extractAllClaims(token: String): Claims {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+    }
+}
