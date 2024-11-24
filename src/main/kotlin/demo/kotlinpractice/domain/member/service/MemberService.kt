1616f91e350f939b8b28138cbfe7bb5dee1035b4
노정완 <jeongwan.noh@gmail.com>
2024-11-24 18:19:55 +0900

    feat: 비밀번호 인코딩 로직 적용

--- src/main/kotlin/demo/kotlinpractice/domain/member/service/MemberService.kt
+++ src/main/kotlin/demo/kotlinpractice/domain/member/service/MemberService.kt
@@ -5,20 +5,21 @@ import demo.kotlinpractice.domain.member.domain.repository.MemberRepository
 import demo.kotlinpractice.domain.member.port.`in`.MemberUseCase
 import demo.kotlinpractice.domain.member.presentation.dto.request.MemberCreateRequest
 import demo.kotlinpractice.domain.member.presentation.dto.request.MemberUpdateRequest
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class MemberService(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder
 ): MemberUseCase {
-
     @Transactional
     override fun createMember(request: MemberCreateRequest): Member {
         val member = Member(
             id = 0,
             name = request.name,
-            password = request.password
+            password = passwordEncoder.encode(request.password)
         )
         return memberRepository.save(member)
     }
