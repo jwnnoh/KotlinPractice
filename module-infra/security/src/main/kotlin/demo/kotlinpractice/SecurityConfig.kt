43f478464d40e4a3cb21b7aa38377375e4bdd7b6
노정완 <jeongwan.noh@gmail.com>
2025-01-16 21:45:09 +0900

    refactor: SecurityConfig DSL 적용

--- module-infra/security/src/main/kotlin/demo/kotlinpractice/SecurityConfig.kt
+++ module-infra/security/src/main/kotlin/demo/kotlinpractice/SecurityConfig.kt
@@ -1,33 +1,56 @@
 package demo.kotlinpractice
 
+import demo.kotlinpractice.jwt.AuthenticationService
+import demo.kotlinpractice.jwt.JwtAuthorizationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val authenticationService: AuthenticationService,
+) {
     @Bean
-    fun securityFilterChain(
-        http: HttpSecurity,
-        jwtAuthenticationFilter: JwtAuthorizationFilter
-    ): DefaultSecurityFilterChain {
-        http
-            .csrf { it.disable() }
-            .authorizeHttpRequests {
-                it
-                    .requestMatchers("/**")
-                    .permitAll()
-                    .anyRequest()
-                    .permitAll()
+    fun SecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize("/swagger-ui/**", permitAll)
+                authorize("/v3/**", permitAll)
+                authorize("/health-check", permitAll)
+                authorize(anyRequest, permitAll)
             }
-            .sessionManagement {
-                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            }
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+        }
+
+        http {
+            csrf { disable() }
+            formLogin { disable() }
+            httpBasic { disable() }
+            sessionManagement { SessionCreationPolicy.STATELESS }
+            cors { disable() }
+        }
+
+        http {
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(
+                filter = JwtAuthorizationFilter(authenticationService)
+            )
+        }
         return http.build()
     }
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
-    }
+    fun passwordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
+
 
     @Bean
     fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
